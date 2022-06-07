91a1ced3cd32d3fa8d9198bfe116e1e2575ea6ff
Maggie Saperstein <maggie.saperstein@gmail.com>
2022-06-07 08:29:09 -0500

    commit tree de04182467f151b03fb2d5a0d0e12b5106f73d1c

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,12 @@
 import React from "react";
-import howManyParks from "./parks/howManyParks";
+import MesaVerde from './parks/MesaVerde';
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
-
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+    <MesaVerde />
+    </div>
+  )
 }
+
+export default ColoradoStateParks;
